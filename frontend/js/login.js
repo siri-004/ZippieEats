221ac07d48d5b4da6a93b5135090656daed35b70
Siri Chandana <chandanasiri004@gmail.com>
2026-02-19 14:05:49 +0530

    Fixed login API URL for production

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -12,7 +12,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/user/login", {
+    const res = await fetch("/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
